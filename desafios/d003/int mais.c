ba5dd13a8e51e960f3c2dbece2030b0fd05cafeb
Teless0 <lfelipet01@gmail.com>
2024-11-24 19:37:16 -0300

    comit exercicios

---
+++ desafios/d003/int mais.c
@@ -0,0 +1,18 @@
+int main(){
+  int i, c;
+  
+  
+  printf("quero morrer, essa vida não tem mais sentido, nao vejo beneficios em estar vivo");
+  
+  return 0;
+  system pause;
+
+
+
+
+
+
+
+
+
+}
